62ce16ff4c18cceb0da4b704c90ff7394e417f4e
Melonisjy <dhy2457@naver.com>
2023-04-28 14:34:08 +0900

    import Tobeupdated component

--- src/pages/mynft.jsx
+++ src/pages/mynft.jsx
@@ -1,5 +1,5 @@
 import MynftHeader from "../components/MynftHeader";
-import { HiWrenchScrewdriver } from "react-icons/hi2";
+import ToBeUpdated from "../components/ToBeUpdate";
 
 const Mynft = ({ myNftBalance }) => {
   return (
@@ -9,12 +9,7 @@ const Mynft = ({ myNftBalance }) => {
         <div className="text-white flex justify-start font-title p-2 pl-12">
           Number of my NFT: {myNftBalance}
         </div>
-        <div className="min-h-screen flex justify-center items-center rounded-full pb-44">
-          <div className="text-7xl font-display flex gap-8 text-gray-200 font-bold border-8 border-gray-200 p-20 rounded-full">
-            <HiWrenchScrewdriver />
-            To be updated.
-          </div>
-        </div>
+        <ToBeUpdated />
       </div>
     </>
   );
