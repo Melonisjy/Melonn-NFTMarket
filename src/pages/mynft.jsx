16e6fc831778d08e7aaf79d74a37f391d2752ffc
Melonisjy <dhy2457@naver.com>
2023-04-28 14:21:43 +0900

    add {To be updated}

--- src/pages/mynft.jsx
+++ src/pages/mynft.jsx
@@ -1,11 +1,20 @@
 import MynftHeader from "../components/MynftHeader";
+import { HiWrenchScrewdriver } from "react-icons/hi2";
 
 const Mynft = ({ myNftBalance }) => {
   return (
     <>
       <MynftHeader />
       <div className="min-h-screen bg-black">
-        <div className="text-white">보유한 nft 개수: {myNftBalance}</div>
+        <div className="text-white flex justify-start font-title p-2 pl-12">
+          Number of my NFT: {myNftBalance}
+        </div>
+        <div className="min-h-screen flex justify-center items-center rounded-full pb-44">
+          <div className="text-7xl font-display flex gap-8 text-gray-200 font-bold border-8 border-gray-200 p-20 rounded-full">
+            <HiWrenchScrewdriver />
+            To be updated.
+          </div>
+        </div>
       </div>
     </>
   );
