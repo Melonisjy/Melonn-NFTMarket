661bbf55915cff2ff8676b39365141d8addc3a1f
Melonisjy <dhy2457@naver.com>
2023-04-28 14:34:30 +0900

    ToBeUpdated Component

---
+++ src/components/ToBeUpdate.jsx
@@ -0,0 +1,14 @@
+import { HiWrenchScrewdriver } from "react-icons/hi2";
+
+const ToBeUpdated = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center rounded-full pb-44">
+      <div className="text-7xl font-display flex gap-8 text-gray-200 font-bold border-8 border-gray-200 p-20 rounded-full">
+        <HiWrenchScrewdriver />
+        To be updated.
+      </div>
+    </div>
+  );
+};
+
+export default ToBeUpdated;
